c5f06dd265478b83198b407a56d668eddb6d0801
TarkanV <carchaido@gmail.com>
2023-03-13 15:47:16 +0100

    Set confirm password feature and style inputs

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 let h1 = document.querySelector("h1");
 
-let property = getComputedStyle(h1).getPropertyValue('font-size');
-console.info("The value is : " + property);
+let pass = document.getElementById("password");
+let passConfirm = document.getElementById("confirm-password");
+
+passConfirm.addEventListener("input", ()=>{
+    if(pass.value !== passConfirm.value && passConfirm.value != "")
+        passConfirm.setCustomValidity("Passwords do not match.");
+    else 
+        passConfirm.setCustomValidity("");
+});
+let inputs = document.querySelectorAll("input");
+
+let submit = document.querySelector("#submit");
+
+submit.addEventListener("click", () =>{
+    console.log("do something");
+    inputs.forEach((input) =>{
+        if(input.validity.valid != true)
+         input.classList.toggle("valid-check", true);
+    });
+});
+
